7dd8bd89302234e9c9ec433f5b67e846a1e3371e
Anthony Suárez <neolight1010@gmail.com>
2021-06-11 21:49:40 -0500

    Add pico_y_placa index page.

--- src/pico_y_placa_app/views.py
+++ src/pico_y_placa_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    return render(request, "pico_y_placa_app/index.html")
